bc78272751c9bdc4701e41b0c64a8d7b82bfd50c
Deian Vladkov <deian.vladkov@gmail.com>
2017-12-28 22:48:14 +0200

    Added event listeners

--- src/js/tic-tac-toe.js
+++ src/js/tic-tac-toe.js
@@ -1,1 +1,42 @@
 // Tic Tac Toe
+
+
+function ticTacToe() {    
+    this.moves = [];
+    this.initEvents();    
+}
+
+
+ticTacToe.prototype.initEvents = function (buttons) {
+    //console.log(settings);
+    document.getElementById("pos-1").addEventListener("click", function () {   
+        console.log('pos1 clicked');
+    });
+    document.getElementById("pos-2").addEventListener("click", function () {   
+        console.log('pos2 clicked');
+    });
+    document.getElementById("pos-3").addEventListener("click", function () {   
+        console.log('pos3 clicked');
+    });
+    document.getElementById("pos-4").addEventListener("click", function () {   
+        console.log('pos4 clicked');
+    });
+    document.getElementById("pos-5").addEventListener("click", function () {   
+        console.log('pos5 clicked');
+    });
+    document.getElementById("pos-6").addEventListener("click", function () {   
+        console.log('pos6 clicked');       
+    });
+    document.getElementById("pos-7").addEventListener("click", function () {   
+        console.log('pos7 clicked');
+    });
+    document.getElementById("pos-8").addEventListener("click", function () {   
+        console.log('pos8 clicked');
+    });
+    document.getElementById("pos-9").addEventListener("click", function () {   
+        console.log('pos9 clicked');       
+    });    
+}
+
+var game = new ticTacToe();
+
