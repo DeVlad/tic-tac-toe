4e783fd8804582cbf226b3fdf90dadf6b3c89944
Deian Vladkov <deian.vladkov@gmail.com>
2017-12-29 22:50:29 +0200

    Display player move

--- src/js/tic-tac-toe.js
+++ src/js/tic-tac-toe.js
@@ -1,42 +1,28 @@
 // Tic Tac Toe
 
-
 function ticTacToe() {    
-    this.moves = [];
-    this.initEvents();    
+    this.moves = [];     
+    this.player = 'X';
+    this.computer = '0';
+    this.initEvents();
 }
 
+ticTacToe.prototype.displayMove = function (position, player) {
+    //console.log('Display Move:', position, player);
+    document.getElementById(position).innerHTML = player;
+};
 
-ticTacToe.prototype.initEvents = function (buttons) {
-    //console.log(settings);
-    document.getElementById("pos-1").addEventListener("click", function () {   
-        console.log('pos1 clicked');
-    });
-    document.getElementById("pos-2").addEventListener("click", function () {   
-        console.log('pos2 clicked');
-    });
-    document.getElementById("pos-3").addEventListener("click", function () {   
-        console.log('pos3 clicked');
-    });
-    document.getElementById("pos-4").addEventListener("click", function () {   
-        console.log('pos4 clicked');
-    });
-    document.getElementById("pos-5").addEventListener("click", function () {   
-        console.log('pos5 clicked');
-    });
-    document.getElementById("pos-6").addEventListener("click", function () {   
-        console.log('pos6 clicked');       
-    });
-    document.getElementById("pos-7").addEventListener("click", function () {   
-        console.log('pos7 clicked');
-    });
-    document.getElementById("pos-8").addEventListener("click", function () {   
-        console.log('pos8 clicked');
-    });
-    document.getElementById("pos-9").addEventListener("click", function () {   
-        console.log('pos9 clicked');       
-    });    
-}
+ticTacToe.prototype.initEvents = function (buttons) {    
+    document.getElementById("pos-1").addEventListener("click", this.displayMove.bind(this, "pos-1", this.player));
+    document.getElementById("pos-2").addEventListener("click", this.displayMove.bind(this, "pos-2", this.player));
+    document.getElementById("pos-3").addEventListener("click", this.displayMove.bind(this, "pos-3", this.player));
+    document.getElementById("pos-4").addEventListener("click", this.displayMove.bind(this, "pos-4", this.player));
+    document.getElementById("pos-5").addEventListener("click", this.displayMove.bind(this, "pos-5", this.player));
+    document.getElementById("pos-6").addEventListener("click", this.displayMove.bind(this, "pos-6", this.player));
+    document.getElementById("pos-7").addEventListener("click", this.displayMove.bind(this, "pos-7", this.player));
+    document.getElementById("pos-8").addEventListener("click", this.displayMove.bind(this, "pos-8", this.player));
+    document.getElementById("pos-9").addEventListener("click", this.displayMove.bind(this, "pos-9", this.player));
+};
 
 var game = new ticTacToe();
 
